4c8b0d4ea8af8edfe683b144ec4a0565f4a3aa49
Shellvi-anna <shellviana.47@smk.belajar.id>
2025-02-10 21:04:12 +0700

    tambah data

--- kategori.php
+++ kategori.php
@@ -68,3 +68,27 @@ while ($category = mysqli_fetch_assoc($exec)):
 </div>
 </div>
 <?php include '.includes/footer.php'; ?>
+
+     <!--modal untuk tambah data kategory --> 
+<div class="modal fade" id="addCategory" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Tambah Data</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <form action="proses_kategori.php" method="POST">
+                    <div class="mb-3">
+                        <label for="namakategori" class="form-label">Nama Kategori</label>
+                        <input type="text" class="form-control" name="category_name" required>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
